c563df4b2d94b3e5b89418797bfb3a87a4066f3c
Jatin Bhakta <bhaktaj7@students.rowan.edu>
2019-10-24 21:54:24 -0400

    Update objectExamples.js

--- objectExamples.js
+++ objectExamples.js
@@ -9,4 +9,13 @@ console.log(target);
 
 console.log(returnedTarget);
 // expected output: Object { a: 1, b: 4, c: 5 }
+
+// merging object
+var o1 = { a: 1 };
+var o2 = { b: 2 };
+var o3 = { c: 3 };
+
+var obj = Object.assign(o1, o2, o3);
+console.log(obj); // { a: 1, b: 2, c: 3 }
+console.log(o1);  // { a: 1, b: 2, c: 3 }, target object itself is changed.
  
